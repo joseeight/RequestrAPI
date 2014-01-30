c26b3ba10eaf3f938a384f2fc8078271449ae4ea
Joseph McCarthy <jmccarthy14@gmail.com>
2014-01-30 13:17:11 +0100

    added token feature

--- src/handler/xhr-handlers.js
+++ src/handler/xhr-handlers.js
@@ -1,24 +1,33 @@
 var schemaInspector = require('schema-inspector');
 var apiSchemas = require('../model/api-schemas.js');
 var request = require('request');
+var crypto = require('crypto');
 
 // Request Handlers
 function statusRequest(req, res){
     res.send('ok!');
 }
 
+// todo (jmccarthy14@) validate responses to prevent 'undefined' errors
 function assetRequest(req, res){
     var requestedResourceBundle = req.body;
     var returnedAssets = [];
     var urlsToProcess = requestedResourceBundle.Urls.length;
     requestedResourceBundle.Urls.forEach(function(resource) {
         request(resource.Url, function (error, response, body) {
-            returnedAssets.push({
+            var data = resource.Type == 'STRING' ? body : new Buffer(body).toString('base64');
+            var shasum = crypto.createHash('sha1').update(data);
+            var returnedAsset = {
                 Url:            resource.Url,
-                Data:           resource.Type == 'STRING' ? body : new Buffer(body).toString('base64'),
-                Type:           resource.Type == 'STRING' ? resource.Type : 'DATA_URI',
-                ContentType:    response.headers['content-type'].split(';')[0]
-            });
+                Token:          shasum.digest('hex'),
+                ContentType:    response.headers['content-type'].split(';')[0],
+                Type:           resource.Type == 'STRING' ? resource.Type : 'DATA_URI'
+            };
+
+            if(returnedAsset.Token != resource.Token) {
+                returnedAsset.Data = data;
+            }
+            returnedAssets.push(returnedAsset);
             if(--urlsToProcess == 0) {
                 res.send(JSON.stringify(returnedAssets));
             }
