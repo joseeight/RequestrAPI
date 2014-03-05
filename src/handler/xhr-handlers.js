5e33b57a002f0fba8b8d0ab6afe246f6a5307a5d
Joseph McCarthy <jmccarthy14@gmail.com>
2014-03-04 20:31:56 -0800

    style fixes

--- src/handler/xhr-handlers.js
+++ src/handler/xhr-handlers.js
@@ -3,36 +3,47 @@ var apiSchemas = require('../model/api-schemas.js');
 var request = require('request');
 var crypto = require('crypto');
 
-// Request Handlers
-function statusRequest(req, res) {
-    res.send('ok!');
+/**
+ * Returns the status of the server.
+ * @param {object} request The express-js request object.
+ * @param (object) response The express-js response object.
+ * @param (object) next The function to call the next filter
+ *     in the chain.
+ */
+function statusRequest(request, response, next) {
+    response.send({
+        Up: true,
+        Service: 'Greenfield'
+    });
 }
 
-// todo (jmccarthy14@) validate responses to prevent 'undefined' errors
-// todo (jmccarthy14@) create header blacklist
-function assetRequest(req, res) {
-    var requestedResourceBundle = req.body;
+// TODO (jmccarthy14@): Validate  the responses to prevent 'undefined' errors.
+// TODO (jmccarthy14@): Create a header blacklist.
+/**
+ * Requests the physical asset from the remote servers.
+ * @param {object} request The express-js request object.
+ * @param (object) response The express-js response object.
+ * @param (object) next The function to call the next filter
+ *     in the chain.
+ */
+function assetRequest(request, response, next) {
+    var requestedResourceBundle = request.body;
     var returnedAssets = [];
     var urlsToProcess = requestedResourceBundle.Urls.length;
-    var options = {
-        headers : {
-            'User-Agent': req.headers['user-agent'],
-            'Origin': req.headers.origin
-        }
-    };
+
     requestedResourceBundle.Urls.forEach(function(resource) {
         var ops = {uri: resource.Url};
         if (resource.Type !== 'STRING') {
           ops.encoding = 'binary';
         }
-        request(ops, function (error, response, body) {
-            // todo (jmc@) error handling
+        request(ops, function (error, assetResponse, body) {
+            // TODO (jmccarthy14@): Add error handling here.
             var data = resource.Type == 'STRING' ? body : new Buffer(body.toString(), 'binary').toString('base64');
             var shasum = crypto.createHash('sha1').update(data);
             var returnedAsset = {
                 Url: resource.Url,
                 Token: shasum.digest('hex'),
-                ContentType: response.headers['content-type'].split(';')[0],
+                ContentType: assetResponse.headers['content-type'].split(';')[0],
                 Type: resource.Type == 'STRING' ? resource.Type : 'DATA_URI'
             };
 
@@ -41,22 +52,28 @@ function assetRequest(req, res) {
             }
             returnedAssets.push(returnedAsset);
             if (--urlsToProcess == 0) {
-                res.send(JSON.stringify({Resources: returnedAssets}));
+                response.send(JSON.stringify({Resources: returnedAssets}));
             }
         })
     });
 }
 
 
-// Utility
-// TODO (jmc@) include this once nodejs is upgraded past 11.2 or patch schema-inspector
+// TODO (jmccarthy14@): Include this once nodejs is upgraded past 11.2 or patch schema-inspector
+/**
+ * A meta-function to be sure that the inputs for all API resources are validated.
+ * @param {function(object,object,object)} requestHandler The actual xhr request handler for the API resource.
+ * @param {object} bodySchema The schema type to match for the inputted
+ * @returns {Function}
+ */
 function validatedRequest(requestHandler, bodySchema) {
-    return function (req, res) {
+    return function (request, response, next) {
         schemaInspector.validate(bodySchema, req.body, function validationResult(err, result) {
             if (err) {
                 console.log(result.format());
+                response.send('Invalid JSON object, be sure it is properly hydrated.');
             } else {
-                requestHandler(req, res);
+                requestHandler(request, response, next);
             }
         });
     };
--- src/server/express-server.js
+++ src/server/express-server.js
@@ -3,15 +3,12 @@ var path = require('path');
 
 var server = express();
 
-
-
 server.configure(function() {
     server.use(express.favicon());
     server.use(express.logger('dev'));
     server.use(express.json());
     server.use(express.urlencoded());
     server.use(express.methodOverride());
-//    server.use(server.router);
     server.use(express.static(path.join(__dirname, 'public')));
 });
 
