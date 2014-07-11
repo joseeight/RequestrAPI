cd9db2845fb489fe84e19a14058c0415e14c032b
Jose Antonio Marquez Russo <jose@eightyearproject.com>
2014-07-10 21:47:47 -0700

    Enabling gzip
    - Fixes issue to allow for compressed json returns
    - Improves performance even further

--- src/server/express-server.js
+++ src/server/express-server.js
@@ -1,25 +1,23 @@
 var express = require('express');
 var path = require('path');
-
 var server = express();
 
-
-
 server.configure(function() {
-    server.use(express.favicon());
-    server.use(express.logger('dev'));
-    server.use(express.json());
-    server.use(express.urlencoded());
-    server.use(express.methodOverride());
-//    server.use(server.router);
-    server.use(express.static(path.join(__dirname, 'public')));
+  server.use(express.favicon());
+  server.use(express.logger('dev'));
+  server.use(express.json());
+  server.use(express.urlencoded());
+  server.use(express.methodOverride());
+  server.use(express.compress());
+  server.use(express.static(path.join(__dirname, 'public')));
 });
 
 if ('development' == server.get('env')) {
-    server.use(express.errorHandler());
+  server.use(express.errorHandler());
 }
 
 
-
+/**
+ * Exporting server module.
+ */
 module.exports = server;
-
